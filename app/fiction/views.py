de2235f65945033b40435e77139ff5656db2dd7d
longzx-9527 <904185259@qq.com>
2018-04-19 11:26:11 +0800

    删除没用文件，修改views完善

--- app/fiction/views.py
+++ app/fiction/views.py
@@ -6,11 +6,11 @@
 from flask import render_template, request, redirect, url_for
 
 from app.xiaoshuo.xiaoshuoSpider import down_fiction_lst, down_fiction_content
-
+from app.xiaoshuo.spider_tools import get_one_page
 from . import fiction
-from ..models import Fiction, Fiction_Content, Fiction_Lst
 import requests
 from bs4 import BeautifulSoup
+from app.models import Fiction, Fiction_Content, Fiction_Lst
 from app import db
 
 
@@ -23,17 +23,22 @@ def book_index():
 
 @fiction.route('/book/list/<f_id>')
 def book_lst(f_id):
+    # 1.获取全部小说
     fictions = Fiction().query.all()
 
     for fiction in fictions:
         if fiction.fiction_id == f_id:
             break
     print(fiction)
+    # 2.获取小说章节列表
     fiction_lst = Fiction_Lst().query.filter_by(fiction_id=f_id).all()
     if len(fiction_lst) == 0:
         print(fiction.fiction_name)
         down_fiction_lst(fiction.fiction_name)
-        return render_template('fiction_error.html', message='暂无此章节信息，请重新刷新下')
+        fiction_lst = Fiction_Lst().query.filter_by(fiction_id=f_id).all()
+        if len(fiction_lst) == 0:
+            return render_template(
+                'fiction_error.html', message='暂无此章节信息，请重新刷新下')
 
     fiction_name = fiction_lst[0].fiction_name
     return render_template(
@@ -67,7 +72,7 @@ def fiction_content():
         fiction_id=fic_id, fiction_url=f_url).first()
     if fiction_contents is None:
         print('fiction_real_url={}'.format(fiction_lst.fiction_real_url))
-        r = requests.get(fiction_lst.fiction_real_url)
+
         down_fiction_content(fiction_lst.fiction_real_url)
         print('fiction_id={} fiction_url={}'.format(fic_id, f_url))
         fiction_contents = Fiction_Content().query.filter_by(
@@ -76,6 +81,7 @@ def fiction_content():
         return render_template('fiction_error.html', message='暂无此章节信息，请重新刷新下')
     print('fiction_contents=', fiction_contents)
     fiction_content = fiction_contents.fiction_content
+    print('sdfewf')
     return render_template(
         'fiction.html',
         f_id=f_id,
@@ -91,6 +97,7 @@ def f_search():
     print('收到输入：', f_name)
     # 1.查询数据库存在记录
     fictions = Fiction().query.all()
+    fiction = None
     for x in fictions:
         if f_name in x.fiction_name:
             fiction = x
@@ -99,7 +106,7 @@ def f_search():
     if fiction:
         fiction_lst = Fiction_Lst().query.filter_by(
             fiction_id=fiction.fiction_id).all()
-        if fiction_lst is None:
+        if len(fiction_lst) == 0:
             down_fiction_lst(f_name)
             fictions = Fiction().query.all()
             print('fictions=', fictions)
